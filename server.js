aea870c1ef5cee102122819113cc0c056309a624
sagarmodhavaniya <47972032+sagarmodhavaniya@users.noreply.github.com>
2019-06-19 15:33:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
-const express=require('express');
-const hbs=require('hbs');
-var app=express();
-const port=process.env.PORT || 3000
-hbs.registerPartials(__dirname+'/viwes/partials');
-app.use(express.static(__dirname+'/public'))
-app.set('view engine','hbs')
-app.use((req,res,next)=>{
-  next();
+let XLSX = require('xlsx');
+const workbook = XLSX.readFile(filename);
+const sheet_name_list = workbook.SheetNames;
+let jsonPagesArray = [];
+sheet_name_list.forEach(function(sheet) {
+        const jsonPage = {
+            name: sheet,
+            content: JSON.parse(JSON.stringify(XLSX.utils.sheet_to_json(workbook.Sheets[sheet],{defval:""})))
+        };
+        jsonPagesArray.push(jsonPage);
+    });
 
-    res.render('maintenance',{
-        copyright:'kevit technology',
-        mono:9624296269
-    })
-// next()
-})
-app.get('/',(req,res)=>{
-    
-        //res.send("<h1>server was running");
-        res.render('home.hbs',{
-            copyright:'kevit technology',
-            mono:9624296269,
-                Title:'Home',
-                welmsg:"Welcome to kevit technology"
-
-        })
-})
-app.get('/about',(req,res)=>{
-    res.render('about.hbs',{
-        copyright:'kevit technology',
-        mono:9624296269,
-    })
-    
-})
-
-
-
-app.listen(port,()=>{
-    console.log(`server are running on ${port} port`)
-});
+console.log(jsonPagesArray[0].content);
